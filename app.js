e71bf33fb2c88a0d81b413e0ddc44f14efce2546
felipe cook <felipepcook@gmail.com>
2022-04-08 04:35:13 -0400

    updated HTML with boilerplate, added object in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Creating my first JS Object :D
+let user = {
+  name: 'John',
+  age: 30,
+
+
+}
+
+// Displays the users age
+console.log(user.age);
+
+
+// Add a property
+user.myCar = 'Volvo';
+
+//Should display 3 properties now.
+console.log(user);
